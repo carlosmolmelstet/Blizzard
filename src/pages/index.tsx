88693dae86166b5d522fcdd26a2324798f0104fd
Carlos Molmelstet <molmelstet2016@gmail.com>
2021-12-05 09:47:02 -0300

    SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetStaticProps } from 'next';
 import React from 'react'
 import Hero from '../components/Hero/Index';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <Hero />
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+
+  return {
+    props: {}
+  }
 }
